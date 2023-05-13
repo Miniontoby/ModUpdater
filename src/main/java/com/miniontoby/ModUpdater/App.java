2287800bccbcea13516d75e15ef9bcf2e3df5271
Miniontoby <tobias.gaarenstroom@gmail.com>
2023-05-13 13:12:07 +0200

    Now compatible with java 8!

--- src/main/java/com/miniontoby/ModUpdater/App.java
+++ src/main/java/com/miniontoby/ModUpdater/App.java
@@ -3,6 +3,8 @@ package com.miniontoby.ModUpdater;
 import com.google.gson.*;
 import java.io.File;
 import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
 import java.text.DateFormat;
 import java.util.Date;
@@ -22,9 +24,23 @@ public class App {
 			java.net.URLConnection uc = new java.net.URL(url).openConnection();
 			uc.setRequestProperty("User-Agent", "ModUpdater - https://github.com/Miniontoby/ModUpdater");
 			uc.setRequestProperty("x-api-key", api_key);
-			java.io.InputStream is = uc.getInputStream();
 
-			String contents = new String(is.readAllBytes());
+			// Java 8
+			BufferedReader in = new BufferedReader(
+				new InputStreamReader(
+					uc.getInputStream()));
+			StringBuilder response = new StringBuilder();
+			String inputLine;
+			while ((inputLine = in.readLine()) != null) response.append(inputLine);
+			in.close();
+			String contents = response.toString();
+
+
+			// Java 11
+			//InputStream is = uc.getInputStream();
+			//String contents = new String(is.readAllBytes());
+
+
 			JsonObject jsonObject = JsonParser.parseString(contents).getAsJsonObject();
 			return jsonObject;
 		} catch (java.io.FileNotFoundException e) {
